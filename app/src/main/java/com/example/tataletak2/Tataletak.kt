809a832684ae84991db7d5d4ab3841c8a8f5c038
HaritsTama <nayotama1818@gmail.com>
2025-10-16 16:32:47 +0700

    Menambahkan warna background pada function TataletakBox

--- app/src/main/java/com/example/tataletak2/Tataletak.kt
+++ app/src/main/java/com/example/tataletak2/Tataletak.kt
@@ -1,6 +1,7 @@
 package com.example.tataletak2
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 
@@ -41,6 +43,7 @@ fun TataletakBox(modifier: Modifier) {
     Box(
         modifier = modifier
             .fillMaxHeight()
+            .background(Color.Cyan)
             .fillMaxWidth(), contentAlignment = Alignment.Center
     ) {
         Text(text = "Box 1")
