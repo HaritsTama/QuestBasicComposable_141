26e426efb7e52e9e6725448ed106ea63bb148609
HaritsTama <nayotama1818@gmail.com>
2025-10-16 16:35:35 +0700

    Menambahkan function TataletakColumnRow

--- app/src/main/java/com/example/tataletak2/MainActivity.kt
+++ app/src/main/java/com/example/tataletak2/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {            Tataletak2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TataletakBox(
+                    TataletakColumnRow(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
