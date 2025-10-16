a96207e213b878c7c0e0e0f33ea899a41b797fb0
HaritsTama <nayotama1818@gmail.com>
2025-10-16 16:17:07 +0700

    Menambahkan function TataletakRow

--- app/src/main/java/com/example/tataletak2/Tataletak.kt
+++ app/src/main/java/com/example/tataletak2/Tataletak.kt
@@ -1,7 +1,10 @@
 package com.example.tataletak2
 
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +15,17 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun TataletakColumn(modifier: Modifier) {
     Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, bottom = 20.dp)) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
+
+@Composable
+fun TataletakRow(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly) {
         Text(text = "Komponen1")
         Text(text = "Komponen2")
         Text(text = "Komponen3")
